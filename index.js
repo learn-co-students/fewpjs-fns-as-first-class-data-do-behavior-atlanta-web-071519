b74dc996b6656eef405db578a8f1853f74966f3e
John Odom <john.osborne.odom@gmail.com>
2019-08-26 13:47:33 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,30 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+// "8:40")).to.equal('Good Morning')
+//     })
+//     it("Returns Good Afternoon between 12pm and 5pm ", () => {
+//       expect(greet("14:21"
+
+// var string = '19:42';
+// var integer = parseInt(string, 10);
+
+//       console.log(integer)
+
+
+function greet(string){
+  let hour = parseInt(string, 10)
+  if (hour < 12){
+    return "Good Morning"
+  } else if (hour>12 && hour<17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string){
+  let greeting = document.getElementById('greeting')
+  greeting.innerText = string
+}
+
